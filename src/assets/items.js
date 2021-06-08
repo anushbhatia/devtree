5738ec9106bde1d4ab220c3b32c241574000610d
Anush Bhatia <anushbhatia1234@gmail.com>
2021-06-09 00:50:30 +0530

    Demo Code

---
+++ src/assets/items.js
@@ -0,0 +1,59 @@
+import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+import appstore from '../assets/appstore.png'
+import twitter from '../assets/twitter.png'
+import dribbble from '../assets/dribbble.png'
+import behance from '../assets/behance.png'
+import github from '../assets/github.png'
+import linkedin from '../assets/linkedin.png'
+
+const items = [{
+    "title": "Website",
+    "subtitle": "Look at my work!",
+    "image": website,
+    "link": "youwebsite.com"
+},
+{
+    "title": "GitHub",
+    "subtitle": "@anushbhatia",
+    "image": github,
+    "link": "https://github.com/anushbhatia"
+},
+{
+    "title": "Instagram",
+    "subtitle": "@anush_bhatia",
+    "image": instagram,
+    "link": "https://instagram.com/anush_bhatia"
+},
+{
+    "title": "Apps",
+    "subtitle": "My App Store Page!",
+    "image": appstore,
+    "link": "#"
+},
+{
+    "title": "Twitter",
+    "subtitle": "@AnushBhatia",
+    "image": twitter,
+    "link": "https://twitter.com/AnushBhatia"
+},
+{
+    "title": "Behance",
+    "subtitle": "@anushbhatia",
+    "image": behance,
+    "link": "https://behance.net/anushbhatia"
+},
+{
+    "title": "Dribbble",
+    "subtitle": "@anushbhatia",
+    "image": dribbble,
+    "link": "https://dribbble.com/anushbhatia"
+},
+{
+    "title": "LinkedIn",
+    "subtitle": "@anushbhatia",
+    "image": linkedin,
+    "link": "https://linkedin.com/in/anushbhatia"
+}]
+
+export default items
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../css/skeleton.css'
+import '../css/normalize.css'
+import '../css/components.css'
+
+function Footer() {
+    return (
+        <div className = "Footer container">
+            <p>Made with <span role="img" aria-label="heart"> ❤️ </span>&nbsp; by Aritro Paul</p>
+        </div>
+    )
+}
+
+export default Footer
